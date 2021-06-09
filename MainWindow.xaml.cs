f1c8dc57e2ff0488d601f239cd31cc41f31998e1
田梓衡 <39756011+IveTian@users.noreply.github.com>
2021-06-09 18:33:41 +0800

    First commit.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace WeChat_DoubleOpen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"D:\应用程序\WeChat\WeChat.exe");
+            System.Diagnostics.Process.Start(@"D:\应用程序\WeChat\WeChat.exe");
+        }
+    }
+}
